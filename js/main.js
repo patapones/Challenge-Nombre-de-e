81ad12511f0e179f3ad2b8b7c58aa5ca0a012481
Neron Kevin <neronkevin@gmail.com>
2022-12-05 14:28:56 +0100

    Ajout des objectifs sur main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
+//Objectif du Challenge
 
+/*
+compter le nombre de "e" dans la liste de tout les cadeaux
+*/
+
+const cadeaux = [
+  "baguette",
+  "voiture",
+  "luge",
+  "pince",
+  "agenda",
+  "camion",
+  "sac",
+  "café",
+  "oreiller",
+  "briquet",
+  "laisse",
+  "iPod",
+  "surligneur",
+  "montre",
+  "siege",
+  "farine",
+  "webflow",
+  "airtable",
+  "bougie",
+  "plante",
+  "veste",
+  "bureau",
+  "dvd",
+  "télé",
+  "pistolet",
+  "casquette",
+  "chien",
+  "PS5",
+  "guitare",
+];
+
+// Résultat attendu
+// Il y a ? "e" dans toute la liste de cadeaux
+
+//TIPS
+//Utiliser la méthode .join() sur le tableau pour créer une grande chaine de caractères
+//Utiliser la méthode .filter() sur le tableau pour garder que les lettres “e” puis regarder la taille du tableau retourné
